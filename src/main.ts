a8d7accb4a5d4db6087c2e50502145e3d086c10b
fyang <42095308+hmfy@users.noreply.github.com>
2024-12-23 14:31:27 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,31 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import { PORT } from '../setting'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path'; // 引入 join 方法来拼接路径
+import * as swaggerUi from 'swagger-ui-express'; // 引入 swagger-ui-express
+import { PORT } from '../setting';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-   // 设置静态文件目录
-  //app.useStaticAssets(join(__dirname, '..', 'public'));  // 设置静态文件路径
-  
+  // 提供 swagger-ui-dist 静态资源
+  app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(undefined, {
+    customCssUrl: '/swagger-ui.css', // 自定义 Swagger UI 的 CSS 文件
+    customJsUrl: '/swagger-ui-bundle.js', // 自定义 Swagger UI 的 JS 文件
+  }));
+
+  // 配置 Swagger 文档
   const options = new DocumentBuilder()
     .setTitle('个人博客')
     .setDescription('接口文档，按入参要求调用')
     .setVersion('1.0.0')
-    .build()
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('docs', app, document)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
 
-  await app.listen(PORT)
+  // 设置 Swagger 路径
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(PORT);
 }
-bootstrap().then((r) => r)
+
+bootstrap().then((r) => r);
